f7c69572321059fd8c880c090ae5a5f646eef30d
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-12-03 15:57:51 +0300

    обновлена админка для сущностей Subject и Branch

--- education/admin.py
+++ education/admin.py
@@ -2,13 +2,20 @@ from django.contrib import admin
 
 from education.models import Subject, Branch, Material
 
-admin.site.register(Subject)
 
-admin.site.register(Branch)
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title',)
+
+
+@admin.register(Branch)
+class BranchAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'subject',)
+    list_filter = ('subject',)
 
 
 @admin.register(Material)
-class MailingLogAdmin(admin.ModelAdmin):
+class MaterialAdmin(admin.ModelAdmin):
     list_display = ('pk', 'title', 'date_of_last_modification', 'branch', 'owner',)
     list_filter = ('branch', 'owner',)
     search_fields = ('title', 'owner')
